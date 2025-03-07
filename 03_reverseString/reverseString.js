96e1f4dc0d2fe1de9629e2d8f3d0da9fff42631b
kadeCeratec <mohamed.kamara@ceratec.eu>
2025-03-07 16:05:32 +0100

    tot 05sumALL.js, 05 moet nog afgewerkt worden

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+   let newWord = string.split("");
+   let reverseWord= newWord.reverse().join("");
 
+   return reverseWord;
+   
 };
 
+reverseString("hello")
 // Do not edit below this line
 module.exports = reverseString;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,15 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    const array =[];
+    let sum;
+    if(num1 > num2){
+        [num1, num2] = [num2, num1];
+    }
+
+    for (let i = num1; i <= num2; i++) {
+        console.log(i);
+    }
 
 };
-
+sumAll(10,5);
 // Do not edit below this line
 module.exports = sumAll;
